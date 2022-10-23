e5ed88a8f1dd1b0f77d86399e0812369f3452b3a
Colin Wren <colinwren@protonmail.com>
2022-10-23 01:23:24 +0100

    Converting to TypeScript Adding transcribe endpoint

--- next.config.js
+++ next.config.js
@@ -2,6 +2,21 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  // webpack: (config, { isServer }) => {
+  //   if (!isServer) {
+  //     config.node = {
+  //       fs: 'empty'
+  //     }
+  //   }
+  //   return config
+  // }
+  webpack: config => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+    }
+    return config
+  }
 }
 
 module.exports = nextConfig
---
+++ pages/api/transcribe.ts
@@ -0,0 +1,37 @@
+import formidable, {Fields, Files} from 'formidable'
+import { readMidiFile, processTrack } from 'midi-to-lsdj'
+
+export const config = {
+  api: {
+    bodyParser: false
+  }
+}
+
+type ProcessedFiles = {
+  fields: Fields,
+  files: Files
+}
+
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    try {
+      const formData = await new Promise<ProcessedFiles>((resolve, reject) => {
+        const form = new formidable.IncomingForm()
+        form.parse(req, (error, fields, files) => {
+          if (error) return reject(error)
+          resolve({ fields, files })
+        })
+      })
+      if (typeof formData.files.file !== 'undefined' && !Array.isArray(formData.files.file)) {
+        const midiData = readMidiFile(formData.files.file.filepath)
+        const trackData = processTrack(midiData.tracks[0], midiData.header.ticksPerBeat)
+        res.status(200).json(JSON.stringify(trackData))
+      }
+    } catch (error) {
+      console.error('Failed to process file', error)
+      res.status(500).json({status: 'fail', message: 'UPLOAD_ERROR'})
+    }
+  } else {
+    res.status(403)
+  }
+}
