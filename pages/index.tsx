3dab0e927e9641c74e905156b39f49fe5ba8817c
Colin Wren <colinwren@protonmail.com>
2022-10-24 04:43:07 +0100

    Fixing text on homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,24 +15,21 @@ export default function Home() {
         <div>
           <h2>Instructions</h2>
           <ul>
-            <li>Upload a MIDI file (loads of caveats below!) via the "Choose File" button</li>
-            <li>Press "Submit File" button to transcribe it into a set of Phrases, Chains and a Song to program into
-              LSDJ
-            </li>
-            <li>Put those Phrases, Chains and Song into LSDJ and press play!</li>
+            <li>{`Upload a MIDI file (loads of caveats below!) via the "Choose File" button`}</li>
+            <li>{`Press "Submit File" button to transcribe it into a set of Phrases, Chains and a Song to program into LSDJ`}</li>
+            <li>{`Put those Phrases, Chains and Song into LSDJ and press play!`}</li>
           </ul>
-          <p>This project is very much a work in progress, so there's probably loads of bugs / quirks.</p>
-          <p>Currently the following limitations are in place:</p>
+          <p>{`This project is very much a work in progress, so there's probably loads of bugs / quirks.`}</p>
+          <p>{`Currently the following limitations are in place:`}</p>
           <ul>
-            <li>Only the first channel in the MIDI file will be processed and that channel will be assigned to PU1 if
-              it's found to be monophonic
+            <li>{`Only the first channel in the MIDI file will be processed and that channel will be assigned to PU1 if it's found to be monophonic`}
             </li>
             {/*<li>If the name of the file has the word "drum" in then it'll be assumed it's a drum track and it'll be*/}
             {/*  polyphonic wave based drums*/}
             {/*</li>*/}
-            <li>The note resolution is currently capped to semi-quavers (16th notes)</li>
-            <li>Only non-common time signatures are handled currently</li>
-            <li>The song isn't stored, so you'll need to re-upload</li>
+            <li>{`The note resolution is currently capped to semi-quavers (16th notes)`}</li>
+            <li>{`Only non-common time signatures are handled currently`}</li>
+            <li>{`The song isn't stored, so you'll need to re-upload`}</li>
           </ul>
         </div>
       </div>
